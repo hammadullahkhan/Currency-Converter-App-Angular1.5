7f62566a9479f16950dbfe11cecc6300bfeb341e
Hammad Khan <hammadullahkhan@gmail.com>
2016-12-06 03:39:18 -0500

    proper indenting and ui changes

--- src/app/components/currency/i18n.js
+++ src/app/components/currency/i18n.js
@@ -2,12 +2,14 @@ exports.EN = {
     "heading": "Currency Converter",
     "subHeading": "Type in amount and select currency:",
     "convertedAmount": "Converted amount:",
-    "disclaimer": "Disclaimer"
+    "disclaimer": "Disclaimer",
+    "exchangeRates": "Exchange Rates "
 }
 
 exports.FR = {
     "heading": "Convertisseur de devises",
     "subHeading": "Saisissez le montant et sélectionnez la devise:",
     "convertedAmount": "Montant converti:",
-    "disclaimer": "Avertissement"
+    "disclaimer": "Avertissement",
+    "exchangeRates": "Taux d'échange "
 }
